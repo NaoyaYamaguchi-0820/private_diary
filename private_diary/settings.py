c90ac90c805241e6b188cacee432f3ecafefbac4
NaoyaYamaguchi <55648796+NaoyaYamaguchi-0820@users.noreply.github.com>
2020-09-13 18:09:48 +0900

    create top page.

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from os import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # ロギング設定
 LOGGING = {
